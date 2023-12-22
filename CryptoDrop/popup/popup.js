738d1db28037f78c40efe9b9c55b18f40f9183e5
Omurbek Kurmanbekov <kurmanbekovomurbek80@gmail.com>
2023-12-22 23:56:17 +0600

    half complete

--- CryptoDrop/popup/popup.js
+++ CryptoDrop/popup/popup.js
@@ -1,58 +1,16 @@
-const airdrops = {
-    upcoming: [
-        {
-            name: "CryptoGalaxy",
-            description: "Раздача токенов для ранних подписчиков новой криптовалюты, ориентированной на космические технологии.",
-            price: null,
-            startDate: "2024-02-01",
-            endDate: "2024-02-15"
-        },
-        {
-            name: "GreenChain",
-            description: "Экологически ориентированный токен, поддерживающий проекты возобновляемой энергии.",
-            price: "0.05",
-            startDate: "2024-03-20",
-            endDate: "2024-04-05"
-        }
-    ],
-    active: [
-        {
-            name: "HealthToken",
-            description: "Токен для поддержки инноваций в области здравоохранения.",
-            price: "0.10",
-            startDate: "2023-12-01",
-            endDate: "2023-12-31"
-        },
-        {
-            name: "EduCoin",
-            description: "Токен для финансирования образовательных проектов и стипендий.",
-            price: "0.02",
-            startDate: "2023-11-15",
-            endDate: "2023-12-30"
-        }
-    ],
-    past: [
-        {
-            name: "ArtToken",
-            description: "Токен для поддержки художников и культурных проектов.",
-            price: "0.08",
-            startDate: "2023-06-01",
-            endDate: "2023-06-15"
-        },
-        {
-            name: "TravelChain",
-            description: "Токен для стимулирования туризма и путешествий.",
-            price: "0.03",
-            startDate: "2023-05-10",
-            endDate: "2023-05-25"
-        }
-    ]
-};
-
 document.addEventListener('DOMContentLoaded', function() {
-    displayAirdrops('upcoming', airdrops.upcoming);
-    displayAirdrops('active', airdrops.active);
-    displayAirdrops('past', airdrops.past);
+    fetch('../js/data.json')
+        .then(response => response.json())
+        .then(data => {
+            displayAirdrops('latest', data.latestAirdrops);
+            displayAirdrops('hottest', data.hottestAirdrops);
+            displayAirdrops('potential', data.potentialAirdrops);
+        })
+        .catch(error => console.error('Ошибка при загрузке airdrops:', error));
+
+    document.getElementById('latest-btn').addEventListener('click', () => toggleAirdrops('latest'));
+    document.getElementById('hottest-btn').addEventListener('click', () => toggleAirdrops('hottest'));
+    document.getElementById('potential-btn').addEventListener('click', () => toggleAirdrops('potential'));
 });
 
 function displayAirdrops(sectionId, airdrops) {
@@ -73,4 +31,12 @@ function displayAirdrops(sectionId, airdrops) {
     } else {
         section.innerHTML = `<div>No ${sectionId} airdrops found.</div>`;
     }
+}
+
+function toggleAirdrops(sectionId) {
+    document.querySelectorAll('.airdrops-section').forEach(section => {
+        section.style.display = 'none';
+    });
+
+    document.getElementById(sectionId).style.display = 'block';
 }
