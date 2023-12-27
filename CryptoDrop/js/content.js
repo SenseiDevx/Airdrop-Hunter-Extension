a3d35e2c4ae6599cb242b4b9973bb5b607ea54d1
Omurbek Kurmanbekov <kurmanbekovomurbek80@gmail.com>
2023-12-27 21:38:11 +0600

    done this extension

--- CryptoDrop/js/content.js
+++ CryptoDrop/js/content.js
@@ -5,23 +5,21 @@ function createPopup() {
     popup.className = 'popup';
 
     popup.innerHTML = `
-        <h1 class="popup-title">Crypto Drop</h1>
-        <div id="airdrop-info" class="popup-info">Checking the current site...</div>
-        <div id="airdrop-history" class="popup-history">Sites with airdrops detected:</div>`;
+        <h1 class="popup-title" style="color: #ffffff; font-size: 22px; font-weight: bold">Crypto Drop</h1>
+        <div id="airdrop-info" class="popup-info" style="color: #ffffff; font-size: 18px; font-weight: 500">Checking the current site...</div>`;
     document.body.appendChild(popup);
-    updateHistory();
 }
 
 function addStyles() {
     const style = document.createElement('style');
     style.textContent = `
         .popup {
-            width: 300px;
-            height: 500px;
+            width: max-content;
+            height: max-content;
             position: fixed;
             bottom: 20px;
             left: 20px;
-            padding: 5px;
+            padding: 10px;
             padding-top: 10px;
             z-index: 9999;
             color: #fff;
@@ -30,64 +28,28 @@ function addStyles() {
             align-items: center;
             border-radius: 10px;
             background-color: #1f73b9;
-        }
-        .popup-title {
-            color: #FFFFFF;
-            margin: 5px;
-        }
-        .popup-info, .popup-history {
-            margin: 10px;
-            color: white;
-        }
-        .popup-link {
-            color: lightblue;
-        }
-        .content-h6 {
-            color: #f4f0f0;
+            text-align: center;
         }
     `;
     document.head.appendChild(style);
 }
 
-
 function updateCurrentSiteInfo(found, url = '') {
     const infoDiv = document.getElementById('airdrop-info');
     if (found) {
-        infoDiv.innerHTML = `<h6 class="content-h6">Airdrops have been discovered on this site:</h6> 
-                             <a class="popup-link" href='${url}' target='_blank'>${url}</a>`;
-
-        chrome.storage.local.get(['visitedSites'], function (result) {
-            const visitedSites = result.visitedSites || [];
-            if (!visitedSites.includes(url)) {
-                visitedSites.push(url);
-                chrome.storage.local.set({ 'visitedSites': visitedSites }, function () {
-                    console.log('Visited sites updated:', visitedSites);
-                });
-            }
-        });
+        infoDiv.innerHTML = `
+            <div class="link-block">
+                <h6 class="content-h6" style="color: #ffffff; font-size: 18px; font-weight: 500">Airdrops have been <br> discovered on this site:</h6> 
+                <a class="popup-link" href='${url}' target='_blank'>${url}</a>
+            </div>`;
     } else {
-        infoDiv.innerHTML = `<h6 class="content-h6">Airdrops are not detected on this site.</h6>`;
+        infoDiv.innerHTML = `<h6 class="content-h6" style="color: #ffffff; font-size: 18px; font-weight: 500">Airdrops are not detected on this site.</h6>`;
     }
-}
-
-
-function updateHistory() {
-    chrome.storage.local.get(['visitedSites'], function (result) {
-        const historyDiv = document.getElementById('airdrop-history');
-        if (result.visitedSites && result.visitedSites.length > 0) {
-            const links = result.visitedSites.map(site => `<a href='${site}' target='_blank'>${site}</a>`).join('<br>');
-            historyDiv.innerHTML = 'Sites with airdrops detected:<br>' + links;
-        } else {
-            historyDiv.innerHTML = `<h6> Sites with detected airdrops: none</h6>`;
-        }
-    });
 }
 
 function searchForKeywords(text) {
     return keywords.some(keyword => text.includes(keyword));
 }
-
-
 
 function checkForAirdrops() {
     if (!window.location.href.includes("google.com")) {
@@ -107,7 +69,6 @@ function checkForAirdrops() {
         updateCurrentSiteInfo(false);
     }
 }
-
 
 window.addEventListener('load', () => {
     addStyles();
