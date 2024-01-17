97f7e61f98717ce653b9b7401796526bc223f2ca
Omurbek Kurmanbekov <kurmanbekovomurbek80@gmail.com>
2024-01-17 20:06:50 +0600

    fix bugs

--- CryptoScout/js/background.js
+++ CryptoScout/js/background.js
@@ -16,7 +16,6 @@ chrome.runtime.onInstalled.addListener(function (details) {
     }
 });
 
-
 chrome.alarms.create("checkForAirdrops", {periodInMinutes: 5});
 chrome.alarms.onAlarm.addListener((alarm) => {
     if (alarm.name === "checkForAirdrops") {
@@ -35,39 +34,6 @@ chrome.runtime.onInstalled.addListener(function (details) {
         chrome.alarms.create("postInstall", { delayInMinutes: 1 });
     }
 });
-
-chrome.alarms.onAlarm.addListener((alarm) => {
-    console.log("Alarm triggered", alarm);
-    if (alarm.name === "postInstall") {
-        chrome.notifications.create('cookieTrackingAlert', {
-            type: 'basic',
-            iconUrl: "../assets/images/icon.png",
-            title: 'Privacy Notice',
-            message: 'This extension tracks cookies for functionality purposes. Please review our privacy policy for more details.'
-        });
-    }
-});
-
-
-chrome.tabs.query({}, function (tabs) {
-    for (let tab of tabs) {
-        console.log(tab.url, tab.title);
-    }
-});
-
-chrome.storage.local.set({key: "value"}, function () {
-    if (chrome.runtime.lastError) {
-        console.log("Error when saving:", chrome.runtime.lastError);
-    } else {
-        console.log("The value has been successfully saved.");
-    }
-});
-
-
-chrome.storage.onChanged.addListener(function (changes, areaName) {
-    console.log(changes, areaName);
-});
-
 
 chrome.runtime.onInstalled.addListener(() => {
     chrome.declarativeNetRequest.setExtensionActionOptions({displayActionCountAsBadgeText: true});
